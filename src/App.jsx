968076b54fc509f243a7b8ab36c7fd417f65c9ab
fawad526 <fawadiqbal274@gmail.com>
2022-07-05 15:03:55 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+ //import ProductList from "./components/ProductList";
+// import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+ //import Login from "./pages/Login";
+//import Product from "./pages/Product";
+// import Register from "./pages/Register";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return(<div> <Home/></div>);
 };
 
 export default App;
